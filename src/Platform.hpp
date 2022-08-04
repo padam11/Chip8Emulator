1be6523d2c28f15a1522e060ad33d874540c4878
Rohan Buch <buchrohand2@gmail.com>
2022-08-03 18:42:45 -0700

    final changes before audio implementation

---
+++ src/Platform.hpp
@@ -0,0 +1,21 @@
+#pragma once
+//use sdl to render the rom game.
+#include <cstdint>
+
+class SDL_Window;
+class SDL_Renderer;
+class SDL_Texture;
+
+class Platform
+{
+    public:
+        Platform(char const* title, int windowWidth, int windowHeight, int textureWidth, int textureHeight);
+        ~Platform();
+        void Update(void const* buffer, int pitch);
+        bool ProcessInput(uint8_t* keys);
+        void audio_callback(void* userdata, uint8_t* stream, int bytes);
+    private:
+        SDL_Window* window;
+        SDL_Renderer* renderer;
+        SDL_Texture* texture;
+};
---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+#include "Chip8.hpp"
+#include "Platform.hpp"
+#include <SDL2/SDL.h>
+#include <chrono>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    if (argc != 4)
+    {
+        std:: cerr << "Usage: " << argv[0] << " <Scale> <Delay> <Rom>\n" << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+
+    int videoScale = std::stoi(argv[1]);
+    int cycleDelay = std::stoi(argv[2]);
+    char const* romFilename = argv[3];
+
+    Platform platform("CHIP8", videoScale * VIDEO_WIDTH, videoScale * VIDEO_HEIGHT, VIDEO_WIDTH, VIDEO_HEIGHT);
+
+    Chip8 chip8;
+    chip8.LoadRom(romFilename);
+
+    int videoPitch = sizeof(chip8.video[0] * VIDEO_WIDTH);
+
+    auto lastCycleTime = std::chrono::high_resolution_clock::now();
+    bool quit = false;
+
+    while (!quit)
+    {
+        quit = platform.ProcessInput(chip8.keypad);
+
+        auto currentTime = std::chrono::high_resolution_clock::now();
+
+        float dt = std::chrono::duration<float, std::chrono::milliseconds::period>(currentTime - lastCycleTime).count();
+
+        if (dt > cycleDelay)
+        {
+            lastCycleTime = currentTime;
+            chip8.Cycle();
+            platform.Update(chip8.video, videoPitch);
+        }
+    }
+    return 0;
+}
