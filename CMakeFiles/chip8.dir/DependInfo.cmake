e303cb19d0fec7d2c1bb7a6bab436fa3ed3a65ab
Rohan Buch <buchrohand2@gmail.com>
2022-08-03 21:13:33 -0700

    make

---
+++ CMakeFiles/chip8.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/rohanbuch/Chip8Emulator/src/Chip8.cpp" "CMakeFiles/chip8.dir/src/Chip8.cpp.o" "gcc" "CMakeFiles/chip8.dir/src/Chip8.cpp.o.d"
+  "/Users/rohanbuch/Chip8Emulator/src/Platform.cpp" "CMakeFiles/chip8.dir/src/Platform.cpp.o" "gcc" "CMakeFiles/chip8.dir/src/Platform.cpp.o.d"
+  "/Users/rohanbuch/Chip8Emulator/src/main.cpp" "CMakeFiles/chip8.dir/src/main.cpp.o" "gcc" "CMakeFiles/chip8.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/chip8.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chip8.dir/src/Chip8.cpp.o"
+  "CMakeFiles/chip8.dir/src/Chip8.cpp.o.d"
+  "CMakeFiles/chip8.dir/src/Platform.cpp.o"
+  "CMakeFiles/chip8.dir/src/Platform.cpp.o.d"
+  "CMakeFiles/chip8.dir/src/main.cpp.o"
+  "CMakeFiles/chip8.dir/src/main.cpp.o.d"
+  "chip8"
+  "chip8.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chip8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
